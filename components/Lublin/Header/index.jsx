607e61d46509b255dcc3b6d48f9fe41dd04628e9
sebastian-lucjan <sebastian.lucjan@gmail.com>
2023-01-08 09:59:26 +0100

    Delete ennecessary code

--- components/Lublin/Header/index.jsx
+++ components/Lublin/Header/index.jsx
@@ -1,6 +1,4 @@
 import Link from 'next/link';
-// import Image from 'next/image';
-// import labelLublin from 'public/images/label-lublin.png';
 
 export default function HeaderLublin() {
   return (
