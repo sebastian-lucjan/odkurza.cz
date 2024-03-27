90f8ae65e1fb4565b01d621dab2c120b035c7708
sebastian-lucjan <sebastian.lucjan@gmail.com>
2024-03-27 16:39:07 +0100

    Add: Hero base components

--- data/citiesData.js
+++ data/citiesData.js
@@ -3,8 +3,6 @@ const cityData = {
   mail: 'lublin@odkurza.cz',
   mobile: '602446335',
   address: 'ul.Skrzetuskiego 8 (LSM), Lublin',
-  // lublin: { cityName: 'Lublin', mail: 'lublin@odkurza.cz', mobile: '602446335', address: 'ul.Skrzetuskiego 8 (LSM), Lublin' },
-  // wroclaw: { cityName: 'Wrocław', mail: 'wroclaw@odkurza.cz', mobile: '793119273', address: 'ul. Szkocka 75/2, Wrocław' },
 };
 
 export default cityData;
---
+++ data/homepageData.js
@@ -0,0 +1,15 @@
+import vacuumCleaner from 'public/images/odkurzacz-pioracy-lublin.jpeg';
+
+export const homepageData = {
+  hero: {
+    heading: {
+      mainText: 'Wypożyczalnia',
+      highlightedText: 'odkurzaczy Lublin',
+    },
+
+    image: {
+      src: vacuumCleaner?.src,
+      alt: 'Zdjęcia odkurzacza piorącego Karcher',
+    },
+  },
+};
