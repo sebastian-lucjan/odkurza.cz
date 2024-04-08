8b674fe90c3e5d221f98b92c9cf145537c7dda50
sebastian-lucjan <sebastian.lucjan@gmail.com>
2024-04-08 10:30:25 +0200

    Add: jest and tests for addSpaces function

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 ## Project Under Refactoring
 Currently implemented changes:
 - 🌕🌕🌕️🌕️🌑️  cleaning and updating the code
+- 🌕️🌑️🌑️🌑️🌑️  unit tests
 - 🌕🌕🌑️🌑️🌑️  readme and simple documentation
-- 🌑️🌑️🌑️🌑️🌑️  unit tests
 - 🌑️🌑️🌑️🌑️🌑️  Figma design file
 
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
---
+++ app/lib/utils/utils.test.ts
@@ -0,0 +1,34 @@
+import { addSpaces } from 'app/lib/utils';
+import { MobileNumber } from 'app/types/types';
+
+describe('addSpaces', () => {
+  it('should add spaces after every 3 digits', () => {
+    const phoneNumber: MobileNumber = 123456789;
+    const formattedNumber = addSpaces(phoneNumber);
+    expect(formattedNumber).toBe('123 456 789');
+  });
+
+  it('handles shorter numbers correctly', () => {
+    const phoneNumber: MobileNumber = 12345;
+    const formattedNumber = addSpaces(phoneNumber);
+    expect(formattedNumber).toBe('123 45');
+  });
+
+  it('handles longer numbers correctly', () => {
+    const phoneNumber: MobileNumber = 1234567890123;
+    const formattedNumber = addSpaces(phoneNumber);
+    expect(formattedNumber).toBe('123 456 789 012 3');
+  });
+
+  it('should return the original number if less than 4 digits', () => {
+    const phoneNumber: MobileNumber = 123;
+    const formattedNumber = addSpaces(phoneNumber);
+    expect(formattedNumber).toBe('123');
+  });
+
+  it('handles edge case with no digits', () => {
+    const phoneNumber: MobileNumber = 0;
+    const formattedNumber = addSpaces(phoneNumber);
+    expect(formattedNumber).toBe('0');
+  });
+});
