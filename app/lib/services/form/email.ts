55344670ca0eaf31d0032c966f2426eb75063780
sebastian-lucjan <sebastian.lucjan@gmail.com>
2024-12-27 16:52:52 +0100

    Fix: verified sender

--- app/lib/services/form/email.ts
+++ app/lib/services/form/email.ts
@@ -14,8 +14,8 @@ export async function sendMessage(formData: z.infer<typeof formSchema>) {
     const { name, email, mobile, message, formType } = formData;
 
     const { data, error } = await resend.emails.send({
-      from: 'Formularz kontaktowy - <odkurzacz.wypozycz@gmail.com>',
-      to: ['odkurzacz.wypozycz@gmail.com'],
+      from: 'Formularz kontaktowy - <info@odkurza.cz>',
+      to: ['info@odkurza.cz'],
       subject: 'Odkurza.cz - Formularz kontaktowy',
       text: `Name: ${name}\nEmail: ${email}\nMobile: ${mobile}\nMessage: ${message}`,
       react: EmailTemplate({ name, email, mobile, message, formType: formType as FormTypeType }),
